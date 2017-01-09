3d13deca5bb16590dd011bf8795dd99fc468e78e
Emil Eklund <emileklund@outlook.com>
2017-01-09 09:10:46 +0100

    Fixing Viewmodel Namespaces.

---
+++ TungstenCore/ViewModels/TeacherHomePage.cs
@@ -0,0 +1,13 @@
+﻿namespace TungstenCore.ViewModels
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class TeacherHomePage
+    {
+        public IEnumerable<Group> Groups;
+        public IEnumerable<Assignment> Assignments;
+        public IEnumerable<ScheduleSegment> Schedule;
+        public IEnumerable<Course> Courses;
+    }
+}
