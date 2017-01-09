8cb234a485495037a0bde9c94a26378268f8dd76
Emil Eklund <emileklund@outlook.com>
2017-01-09 09:19:19 +0100

    Removing Potentially Unused Components. Revert if neccesary.

--- TungstenCore/Models/ApplicationUser.cs
+++ TungstenCore/Models/ApplicationUser.cs
@@ -10,14 +10,7 @@ namespace TungstenCore.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-
+        [Required]
         public string Name { get; set; }
 
         [RegularExpression("^([0-9]{6}([-+]).*)$")]
