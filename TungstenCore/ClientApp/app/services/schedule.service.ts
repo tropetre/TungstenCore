f22082f7e8859eefd59637324ac8f6874bedd588
Emil Eklund <emileklund@outlook.com>
2017-01-13 11:48:07 +0100

    Fixed typo.

--- TungstenCore/ClientApp/app/services/schedule.service.ts
+++ TungstenCore/ClientApp/app/services/schedule.service.ts
@@ -8,7 +8,7 @@ export class ScheduleService {
     constructor( @Inject(Http) private http: Http) { }
 
     getSchedule(groupId: string): Observable<ScheduleSegment[]> {
-        return this.http.get('/Home/GetSchedule/' + groupId)
+        return this.http.get('/Group/GetSchedule/' + groupId)
             .do(this.logData)
             .catch(this.handleError)
             .map(this.extractGroups);
